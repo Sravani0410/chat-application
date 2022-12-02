0cf4ff9558c96e8dbaee3383484b1b23d8c0174c
nidhi-extern <97447400+Sravani0410@users.noreply.github.com>
2022-12-02 18:15:48 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,33 @@
-const express= require("express")
+// const express= require("express")
 
-const app=express();
+// const app=express();
+// const socket=require("socket.io")
+// const io=socket(server)
+// io.on('connection', (socket)=>{
+//     console.log('Socket connected successfully')
+//  }
+// );
 
-app.listen(3030,()=>{
-    console.log("listening the port 3030")
-})
+// app.use(express.static('public'));
+
+// app.listen(3030,()=>{
+//     console.log("listening the port 3030")
+// })
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
